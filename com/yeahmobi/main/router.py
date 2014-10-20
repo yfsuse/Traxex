528f4e28cfaceb33e7efd5ad600acd866c51cce8
yfsuse <yfsuse@gmail.com>
2014-10-20 16:46:17 +0800

    add broker table test module

---
+++ com/yeahmobi/main/router.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python
+# --*-- coding:utf-8 --*--
+__author__ = 'jeff.yu'
+
+
+from random import choice
+import sys
+import os
+
+
+class Router(object):
+
+    def __init__(self):
+        self.getDataFromBroker()
+
+    def getBrokerIp(self):
+        return choice(('10.1.5.30', '10.1.5.31'))
+
+    def getDataFromBroker(self):
+        brokerIp = self.getBrokerIp()
+        requestBody = self.getJsonBody()
+        self.dataReceived = []
+        for requestFile in requestBody:
+            sendBody = "curl -X post http://{0}:8080/druid/v2/?pretty -H 'content-type: application/json' -d @{1}".format(brokerIp, requestFile)
+            data = os.popen(sendBody).read()
+            self.dataReceived.append(data)
+
+    def getJsonBody(self):
+        absPath = sys.path[0].replace('main', 'tableRouter/')
+        jsonFile = os.listdir(absPath)
+        return [absPath + f for f in jsonFile if f.startswith('table')]
+
+    @property
+    def check(self):
+        if len(self.dataReceived) == len(set(self.dataReceived)):
+            return True
+        else:
+            return False
+
+if __name__ == '__main__':
+    router = Router()
+    print router.check
+
+
+
+
--- com/yeahmobi/main/script.py
+++ com/yeahmobi/main/script.py
@@ -19,7 +19,7 @@ def main():
     case_list = Case().get_case()
     unix_start, unix_end = get_range()
 
-    for case in case_list:
+    for case in case_list[2:]:
         case = case.replace("-1", unix_start).replace("-2", unix_end).replace(': ', ':').replace(', ', ',')
         print case
         # mysql_rsp = get_data("mysql", case)
@@ -28,8 +28,6 @@ def main():
         druid_data = list_to_json(druid_rsp)
         # data_arrange(mysql_data, druid_data)
         print druid_data
-        break
 
 if __name__ == '__main__':
-    print sys.path[0]
     main()
